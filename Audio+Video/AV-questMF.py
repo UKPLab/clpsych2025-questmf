72111e36be275a1fdd05cfa8c1672599d0ee93c3
Aishik Mandal <50352686+jitaishik@users.noreply.github.com>
2024-07-02 11:54:14 +0200

    Add files via upload

--- Audio+Video/AV-questMF.py
+++ Audio+Video/AV-questMF.py
@@ -21,6 +21,7 @@ def cmdline_args():
     p.add_argument("-v_ckpt", "--video_checkpoint_path", type=str, help="Path to checkpoint for the video model")
     p.add_argument("-av_ckpt", "--av_checkpoint_path", type=str, help="Path to checkpoint for the audio+video model")
     p.add_argument("-m_files", "--missing_video_files",nargs='+', type=int, help="List of file numbers for incomplete video files")
+    p.add_argument("-train","--train_model", action='store_true',help="Wheather to train the model or not")
 
     return (p.parse_args())
 
@@ -450,7 +451,8 @@ if __name__ == '__main__':
     loss_fn = nn.MSELoss()
     mae_loss_fn = nn.L1Loss()
     
-    # train(model, train_dataloader, args.av_checkpoint_path + '-phq' + str(args.question_number) + '-sr-parameters.pt', val_dataloader, epochs=num_epochs, evaluation=True)
+    if args.train_model:
+        train(model, train_dataloader, args.av_checkpoint_path + '-phq' + str(args.question_number) + '-sr-parameters.pt', val_dataloader, epochs=num_epochs, evaluation=True)
     
     # Load trained AV model
     best_lstm_regressor = lstm_regressor(pretrain_vid_model, pretrain_aud_model)
